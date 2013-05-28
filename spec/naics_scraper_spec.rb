a32648bef016d91efb5e22cbd6e9bf360a1f0db7
Dave Guarino <dave@codeforamerica.org>
2013-05-27 17:52:39 -0700

    Got response for 4-digit code

--- spec/naics_scraper_spec.rb
+++ spec/naics_scraper_spec.rb
@@ -14,11 +14,17 @@ describe NaicsScraper, "::get_codes_for_year" do
 end
 
 describe NaicsScraper, "::get_content_for_code" do
-  before do
-    @code_content = NaicsScraper.get_content_for_code(423120, 2012)
-  end
   it "should return the correct description for 423120 in 2012 NAICS" do
+    @code_content = NaicsScraper.get_content_for_code(423120, 2012)
     @code_content.should eq("This industry comprises establishments primarily engaged in the merchant wholesale distribution of motor vehicle supplies, accessories, tools, and equipment; and new motor vehicle parts (except new tires and tubes).")
   end
 end
+
+describe NaicsScraper, "::get_response_for_code" do
+  it "should return a successful response for a 4-digit code (4231 in 2012)" do
+    @response = NaicsScraper.get_response_for_code(4231, 2012)
+    @response.code.should eq(200)
+  end
+end
+
 

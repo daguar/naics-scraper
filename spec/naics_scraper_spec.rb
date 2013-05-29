626d1303b4d3f26b085b306afa394004f096e2d4
Dave Guarino <dave@codeforamerica.org>
2013-05-28 20:30:28 -0700

    Convert @lou's JS-style arrays to Ruby-style ones

--- spec/naics_scraper_spec.rb
+++ spec/naics_scraper_spec.rb
@@ -36,13 +36,11 @@ end
 describe NaicsScraper, "::get_examples_for_code" do
   it "returns a list of illustrative examples for 54119 in 2012" do
     @code_examples = NaicsScraper.get_examples_for_code(54119, 2012)
-    example_array = {
-      "Notary public services",
-      "Process serving services",
-      "Paralegal services",
-      "Real estate settlement offices",
-      "Patent agent services (i.e., patent filing and searching services) Real estate title abstract companies"
-    }
+    example_array = ["Notary public services", \
+      "Process serving services", \
+      "Paralegal services", \
+      "Real estate settlement offices", \
+      "Patent agent services (i.e., patent filing and searching services) Real estate title abstract companies"]
     @code_examples.should eq(example_array)
   end
   it "returns nothing for a code without illustrative examples, e.g. 541120 in 2012" do
@@ -54,21 +52,17 @@ end
 describe NaicsScraper, "::get_crossrefs_for_code" do
   it "returns a list of cross references for 54119 in 2012" do
     @code_crossrefs = NaicsScraper.get_crossrefs_for_code(54119, 2012)
-    example_array = {
-      "Establishments of lawyers and attorneys primarily engaged in the practice of law are classified in Industry 541110, Offices of Lawyers; and",
-      "Establishments of notaries public engaged in activities, such as administering oaths and taking affidavits and depositions, witnessing and certifying signatures on documents, but not empowered to draw and approve legal documents and contracts, are classified in U.S. Industry 541199, All Other Legal Services."
-    }
+    example_array = ["Establishments of lawyers and attorneys primarily engaged in the practice of law are classified in Industry 541110, Offices of Lawyers; and", \
+      "Establishments of notaries public engaged in activities, such as administering oaths and taking affidavits and depositions, witnessing and certifying signatures on documents, but not empowered to draw and approve legal documents and contracts, are classified in U.S. Industry 541199, All Other Legal Services."]
     @code_crossrefs.should eq(example_array)
   end
   it "returns a list of cross references, with a header, for 541211 in 2012" do
     @code_crossrefs = NaicsScraper.get_crossrefs_for_code(541211, 2012)
     # Not sure if this is the best way to store this bit of information, but it's a formatting quirk of the text
-    example_array = {
-      "Establishments primarily engaged in--",
-      "Providing tax return preparation services only--are classified in U.S. Industry 541213, Tax Preparation Services;",
-      "Providing payroll processing services only--are classified in U.S. Industry 541214, Payroll Services; and",
-      "Providing accounting, bookkeeping, and billing services--are classified in U.S. Industry 541219, Other Accounting Services."
-    }
+    example_array = ["Establishments primarily engaged in--", \
+      "Providing tax return preparation services only--are classified in U.S. Industry 541213, Tax Preparation Services;", \
+      "Providing payroll processing services only--are classified in U.S. Industry 541214, Payroll Services; and", \
+      "Providing accounting, bookkeeping, and billing services--are classified in U.S. Industry 541219, Other Accounting Services."]
     @code_crossrefs.should eq(example_array)
   end
   it "returns a single cross reference for 541110 in 2012" do
